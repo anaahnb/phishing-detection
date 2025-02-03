3c64eb37731d7eef9904cb949f6d2bdf89d3477e
Cesar-Roberto <cesar.robert1996@gmail.com>
2025-02-02 22:39:48 -0300

    UPDATE: Modularização do correlation_analysis

--- Analysis/correlation_analysis.py
+++ Analysis/correlation_analysis.py
@@ -1,24 +1,35 @@
+import os
 import pandas as pd
+import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-import numpy as np
-import os
 from DataLoading.data_loader import DataLoader
 
-data_loader = DataLoader()
-df = data_loader.load_data()
+class CorrelationAnalysis:
+    """Classe responsável por gerar e salvar a matriz de correlação das variáveis numéricas."""
 
-df_numeric = df.select_dtypes(include=[np.number])
+    def _init_(self, df: pd.DataFrame, output_dir: str = "Figures"):
+        self.df = df.select_dtypes(include=[np.number])
+        self.output_dir = output_dir
+        self._prepare_output_directory()
 
-corr_matrix = df_numeric.corr()
+    def _prepare_output_directory(self):
+        if not os.path.exists(self.output_dir):
+            os.makedirs(self.output_dir)
 
-output_dir = "Figures"
-if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
+    def plot_correlation_matrix(self):
+        """Gera e salva a matriz de correlação das variáveis numéricas."""
+        corr_matrix = self.df.corr()
+        plt.figure(figsize=(12, 8))
+        sns.heatmap(corr_matrix, cmap='coolwarm', annot=False, linewidths=0.5)
+        plt.title('Matriz de Correlação das Variáveis')
+        plt.savefig(f"{self.output_dir}/correlation_matrix.png", dpi=300, bbox_inches="tight")
+        print(f'A matriz foi salva em {self.output_dir}')
 
-plt.figure(figsize=(12, 8))
-sns.heatmap(corr_matrix, cmap='coolwarm', annot=False, linewidths=0.5)
-plt.title('Matriz de Correlação das Variáveis')
-plt.savefig(f"{output_dir}/correlation_matrix.png", dpi=300, bbox_inches="tight")
-print(f'A matriz foi salva em {output_dir}')
+if _name_ == "_main_":
+    data_loader = DataLoader()
+    df = data_loader.load_data()
+
+    correlation_analysis = CorrelationAnalysis(df)
+    correlation_analysis.plot_correlation_matrix()
 
