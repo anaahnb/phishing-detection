5d0876ba961e3737f0f841f89f4fdff0699c22eb
Ana Julia <alvncar@gmail.com>
2025-02-21 22:55:39 -0300

    add raw database and fix database extract module

--- DataLoading/downloader.py
+++ DataLoading/downloader.py
@@ -1,11 +1,23 @@
+import os
+import shutil
 import kagglehub
 from Settings.keys import ParamsKeys
 
 class DataDownloader:
     @staticmethod
     def download() -> str:
-        """Baixa a versão mais recente do dataset do Kaggle e retorna o caminho dos arquivos."""
+        """Baixa a versão mais recente do dataset do Kaggle e move para a pasta correta."""
 
-        path = kagglehub.dataset_download(ParamsKeys.DATASET_NAME)
-        print(f"Arquivos baixados para: {path}")
-        return path
+        cache_path = kagglehub.dataset_download(ParamsKeys.DATASET_NAME)
+        print(f"Arquivos baixados para: {cache_path}")
+
+        datasets_path = os.path.join(os.path.dirname(__file__), "..", "Datasets", "raw")
+        os.makedirs(datasets_path, exist_ok=True)  # Garante que a pasta existe
+
+        for filename in os.listdir(cache_path):
+            src = os.path.join(cache_path, filename)
+            dst = os.path.join(datasets_path, filename)
+            shutil.move(src, dst)
+
+        print(f"Arquivos movidos para: {datasets_path}")
+        return datasets_path
